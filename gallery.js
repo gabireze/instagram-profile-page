7a08a290d7b4544abdfcc8bf15e80965221063b4
Gabriel de Rezende Gonçalves <gabireze@gmail.com>
2021-03-18 19:37:13 -0300

    making the page dynamic

--- gallery.js
+++ gallery.js
@@ -1,34 +1,70 @@
-let imagens = document.querySelectorAll('.small-img');
+let images = [
+  { 'src': 'https://via.placeholder.com/1080x1350', 'likes': 0 },
+  { 'src': 'https://via.placeholder.com/500', 'likes': 0 },
+  { 'src': 'https://via.placeholder.com/720x1080', 'likes': 0 },
+  { 'src': 'https://via.placeholder.com/293', 'likes': 0 },
+  { 'src': 'https://via.placeholder.com/450', 'likes': 0 },
+  { 'src': 'https://via.placeholder.com/1080x1350', 'likes': 0 },
+];
 let modal = document.querySelector('.modal');
 let modalImg = document.querySelector('#modal-img');
 let modalBg = document.querySelector('#modal-bg');
-let srcVal = "";
+let clickedImage = {}
 
-var thx = '';
+for (let i = 0; i < images.length; i++) {
+  if (!document.getElementById("images")) {
+    var imagesSection = document.createElement("section")
+    imagesSection.setAttribute('id', 'images');
+  }
 
-for (let i = 0; i < imagens.length; i++) {
-  imagens[i].dataset.likes = 0;
-  imagens[i].addEventListener('click', function () {
-    thx = imagens[i];
-    srcVal = imagens[i].getAttribute('src');
-    document.getElementById('likes').value = thx.attributes['data-likes'].value;
-    document.getElementById('likes').innerHTML = thx.attributes['data-likes'].value;
-    modalImg.setAttribute('src', srcVal);
+  var imageDiv = document.createElement("div");
+  imageDiv.className = 'card-img';
+
+  var currentImage = document.createElement("img");
+  currentImage.className = 'small-img';
+  currentImage.setAttribute('src', images[i]['src']);
+
+  var id = hashGenerator();
+  images[i].id = id;
+
+  imageDiv.appendChild(currentImage);
+  imagesSection.appendChild(imageDiv);
+
+  document.getElementById("main").appendChild(imagesSection);
+}
+
+let sectionImages = document.querySelectorAll('.small-img');
+
+for (let i = 0; i < images.length; i++) {
+  sectionImages[i].addEventListener('click', function () {
+    clickedImage = images[i];
+    var srcImage = clickedImage.src;
+    modalImg.setAttribute('src', srcImage);
     modal.classList.toggle('modal-active');
   });
 }
 
 modalBg.addEventListener('click', function () {
   modal.classList.toggle('modal-active');
-  thx = '';
-  console.log(thx)
+  clickedImage = {}
 });
 
-modalImg.addEventListener('dblclick', function () {
-  buttonClick();
-});
+function likePhoto() {
+  ++images.find(image => clickedImage.id === image.id).likes;
+}
 
-function buttonClick() {
-  ++thx.attributes['data-likes'].value;
-  document.getElementById('likes').innerHTML = thx.attributes['data-likes'].value;
+function hashGenerator() {
+  return '_' + Math.random().toString(36).substr(2, 9);
+};
+
+function submitCommentary() {
+  if (!images.find(image => clickedImage.id === image.id).comments) {
+    images.find(image => clickedImage.id === image.id).comments = [];
+  }
+  let commentary = {
+    'id': hashGenerator(),
+    'commentary': document.getElementById("commentary").value
+  }
+  images.find(image => clickedImage.id === image.id).comments.push(commentary);
+  document.getElementById("commentary").value = '';
 }
