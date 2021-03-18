532acb700193558a9d28efd4f7ddf5a6e9dafb18
Gabriel de Rezende Gonçalves <gabireze@gmail.com>
2021-03-18 20:32:38 -0300

    programming dynamic modal

--- gallery.js
+++ gallery.js
@@ -41,6 +41,31 @@ for (let i = 0; i < images.length; i++) {
     var srcImage = clickedImage.src;
     modalImg.setAttribute('src', srcImage);
     modal.classList.toggle('modal-active');
+
+    var modalDiv = document.createElement("div");
+    modalDiv.className = 'modal';
+
+    var modalBgDiv = document.createElement("div");
+    modalBgDiv.setAttribute('id', 'modal-bg');
+
+    var modalContentDiv = document.createElement("div");
+    modalContentDiv.className = 'modal-content';
+
+    var modalImg = document.createElement('img');
+    modalImg.setAttribute('id', 'modal-img');
+    modalImg.setAttribute('src', clickedImage.src);
+
+    var modalAside = document.createElement('aside');
+    modalAside.setAttribute('id', 'modal-aside');
+
+    var modalHeader = document.createElement('header');
+
+    var rowSmall = document.createElement("div");
+    rowSmall.className = 'row small';
+
+    var profileImageSmall = document.createElement('img');
+    profileImageSmall.setAttribute('id', 'profile-image-small');
+    profileImageSmall.setAttribute('src', 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80');
   });
 }
 
